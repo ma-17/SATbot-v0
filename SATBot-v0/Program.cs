0ac8900a6e94c6afa90aadf1257844474bfd15e8
Mary <ma17@sheridancollege.ca>
2020-03-10 15:10:50 -0400

    Init project

---
+++ SATBot-v0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SATBot_v0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
